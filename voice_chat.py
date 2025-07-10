a5614957516f67421ed69dc0a9c311b467c7f7b3
Hossam Elsherbiny <hossam.elsherbiny95@gmail.com>
2025-07-10 17:08:31 +0300

    adding tts feature

--- voice_chat.py
+++ voice_chat.py
@@ -1,11 +1,36 @@
 import whisper
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=api_key)
 
 whisper_model = whisper.load_model("small")
+
 
 def stt(filepath: str) -> str:
     response = whisper_model.transcribe(filepath, fp16=False)
     return response["text"]
 
 
+def tts(text: str, voice: str = "alloy", isDeployed: bool = False):
+    # voices = [alloy, echo, fable, onyx, nova, shimmer]
+    response = client.audio.speech.create(
+        model = "tts-1",
+        voice=voice,
+        input=text
+    )
+    
+    if not isDeployed:
+        with open("output.mp3", "wb") as f:
+            f.write(response.read())
+        
+    return response.read()
+
+
 if __name__ == "__main__":
-    print(stt("input.wav"))
+    text = stt("input.wav")
+    tts(text, "nova")
